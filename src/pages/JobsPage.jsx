80a9b7bc07fc4d3c851891f3edb5e33f5627015e
Oknha <Chaokimhay>
2024-07-10 14:34:21 +0700

    jobs page

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const JobsPage = () => {
+  return (
+    <div>
+      <h1 className="text-center text-blue-600 font-semibold">Job Pages</h1>
+    </div>
+  );
+};
+
+export default JobsPage;
