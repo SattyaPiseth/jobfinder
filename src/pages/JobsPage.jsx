72727a574caae3e8b44fcebe067bcdada014ee9b
Oknha <Chaokimhay>
2024-07-10 14:38:10 +0700

    push to main

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const JobsPage = () => {
   return (
     <div>
-      <h1 className="text-center text-blue-600 font-semibold">Job Pages</h1>
+      <h1 className="mt-20 text-center text-blue-600 font-semibold">
+        Job Pages
+      </h1>
     </div>
   );
 };
