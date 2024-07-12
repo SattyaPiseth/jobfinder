e0e3b4ead0d81bbb6bd0a69990680103ab30e7eb
piseth sattya <pisethsattya33@gmail.com>
2024-07-12 17:13:37 +0700

    testing vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  build: {
+    outDir: 'dist', // Default output directory
+  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://136.228.158.126:50002",
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  // },
+  define: {
+    "process.env": process.env,
+  },
+});
