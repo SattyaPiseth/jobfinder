70786b6ada6077eb24f5ce24fc122b514cd9ba13
piseth sattya <pisethsattya33@gmail.com>
2024-07-12 18:01:10 +0700

    testing

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Default output directory
-  },
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://136.228.158.126:50002",
-  //       changeOrigin: true,
-  //       secure: false,
-  //       rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
+  // build: {
+  //   outDir: 'dist', // Default output directory
   // },
-  define: {
-    "process.env": process.env,
-  },
+  // // server: {
+  // //   proxy: {
+  // //     "/api": {
+  // //       target: "http://136.228.158.126:50002",
+  // //       changeOrigin: true,
+  // //       secure: false,
+  // //       rewrite: (path) => path.replace(/^\/api/, ""),
+  // //     },
+  // //   },
+  // // },
+  // define: {
+  //   "process.env": process.env,
+  // },
 });
