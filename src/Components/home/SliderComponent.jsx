d43a411c35afa18fd68d7ec16a954bfce2c73352
piseth sattya <pisethsattya33@gmail.com>
2024-07-17 03:59:20 +0700

    loading

--- src/Components/home/SliderComponent.jsx
+++ src/Components/home/SliderComponent.jsx
@@ -6,6 +6,8 @@ import Skeleton from "react-loading-skeleton";
 import 'react-loading-skeleton/dist/skeleton.css';
 
 const SliderComponent = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   const imageUrls = [
     "https://cdn.builder.io/api/v1/image/assets/TEMP/c34719a5760b420da93199b245058d19f21dd1681df6e0596cf8bf1de786d308?apiKey=0bc16f2f0d6a46d9b1296b8c9a315482&",
     "https://cdn.builder.io/api/v1/image/assets/TEMP/1b0637bed0518cc6adfec9c73be1365577988d916ebb2ce4986966ab195df3d4?apiKey=0bc16f2f0d6a46d9b1296b8c9a315482&",
@@ -15,78 +17,102 @@ const SliderComponent = () => {
     "https://ibccambodia.com/wp-content/uploads/2023/09/ABA-Logo-Secondary.png.webp",
   ];
 
-  const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
-    const imagePromises = imageUrls.map((url) => {
-      return new Promise((resolve) => {
-        const img = new Image();
-        img.src = url;
-        img.onload = resolve;
-      });
-    });
-
-    Promise.all(imagePromises).then(() => {
+    // Simulating image loading delay
+    const timer = setTimeout(() => {
       setIsLoading(false);
-    });
-  }, [imageUrls]);
+    }, 2000); // Adjust the delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div>
       <section className="product-gallery" data-aos="fade-up-left">
-        <Swiper
-          modules={[Autoplay, Pagination, Scrollbar]}
-          spaceBetween={20}
-          slidesPerView={1}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          loop={true}
-          breakpoints={{
-            640: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 30,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 40,
-            },
-            1280: {
-              slidesPerView: 4,
-              spaceBetween: 50,
-            },
-          }}
-        >
-          {isLoading
-            ? Array(6)
-                .fill(0)
-                .map((_, index) => (
-                  <SwiperSlide key={index} className="h-auto pb-10">
-                    <figure className="flex flex-col grow justify-center w-full bg-white rounded-xl">
-                      <Skeleton className="w-full aspect-[2]" />
-                    </figure>
-                  </SwiperSlide>
-                ))
-            : imageUrls.map((url, index) => (
-                <SwiperSlide key={index} className="h-auto pb-10">
-                  <figure className="flex flex-col grow justify-center w-full bg-white rounded-xl">
-                    <img
-                      loading="lazy"
-                      src={url}
-                      className="w-full aspect-[2]"
-                      alt={`Product image ${index + 1}`}
-                    />
-                  </figure>
-                </SwiperSlide>
-              ))}
-          <div className="swiper-scrollbar mt-8"></div>
-          <div className="swiper-pagination mt-8"></div>
-        </Swiper>
+        {isLoading ? (
+          <Swiper
+            modules={[Autoplay, Pagination, Scrollbar]}
+            spaceBetween={20}
+            slidesPerView={1}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            loop={true}
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              1280: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
+            }}
+          >
+            {imageUrls.map((_, index) => (
+              <SwiperSlide key={index} className="h-auto pb-10">
+                <figure className="flex flex-col grow justify-center w-full bg-white rounded-xl">
+                  <Skeleton height={200} width="100%" className="w-full aspect-[2]" />
+                </figure>
+              </SwiperSlide>
+            ))}
+            <div className="swiper-scrollbar mt-8"></div>
+            <div className="swiper-pagination mt-8"></div>
+          </Swiper>
+        ) : (
+          <Swiper
+            modules={[Autoplay, Pagination, Scrollbar]}
+            spaceBetween={20}
+            slidesPerView={1}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            loop={true}
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              1280: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
+            }}
+          >
+            {imageUrls.map((url, index) => (
+              <SwiperSlide key={index} className="h-auto pb-10">
+                <figure className="flex flex-col grow justify-center w-full bg-white rounded-xl">
+                  <img
+                    loading="lazy"
+                    src={url}
+                    className="w-full aspect-[2]"
+                    alt={`Product image ${index + 1}`}
+                  />
+                </figure>
+              </SwiperSlide>
+            ))}
+            <div className="swiper-scrollbar mt-8"></div>
+            <div className="swiper-pagination mt-8"></div>
+          </Swiper>
+        )}
       </section>
     </div>
   );
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -3,7 +3,7 @@ import ContactUsPage from "../Components/contactUs/ContactUsPage";
 
 export default function ContactUs() {
   return (
-    <div className="mb-10">
+    <div className="mb-10 custom-height">
       <ContactUsPage />
     </div>
   );
