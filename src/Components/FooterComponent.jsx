5d8a931027c074911518b8a190a3b5a0b88ea051
piseth sattya <pisethsattya33@gmail.com>
2024-07-17 13:44:26 +0700

    loading

--- src/Components/FooterComponent.jsx
+++ src/Components/FooterComponent.jsx
@@ -22,12 +22,11 @@ const FooterComponent = ({ isLoading }) => {
   useEffect(() => {
     AOS.init({ duration: 1000 });
 
-    // Simulate a loading delay of 1.5 seconds
     const timer = setTimeout(() => {
       setLocalLoading(false);
     }, 1500);
 
-    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+    return () => clearTimeout(timer);
   }, []);
 
   const isComponentLoading = isLoading || localLoading;
@@ -35,15 +34,24 @@ const FooterComponent = ({ isLoading }) => {
   return (
     <Footer
       container
-      className="w-full py-10 px-5 absolute left-0 right-0 bg-primary-800 dark:bg-primary-900 text-white"
+      className="w-full py-10 px-5 bg-primary-800 dark:bg-primary-900 text-white absolute left-0 right-0 rounded-none"
     >
       <div className="container mx-auto px-4">
-        <div className="flex flex-wrap justify-between gap-4">
+        <div className="flex flex-wrap justify-between items-center gap-8">
           <div className="flex-shrink-0 mb-6 md:mb-0">
             {isComponentLoading ? (
               <div className="flex items-center">
-                <Skeleton circle={true} height={36} width={36} className="animate-pulse mr-3" />
-                <Skeleton height={36} width={100} className="animate-pulse" />
+                <Skeleton
+                  circle={true}
+                  height={36}
+                  width={36}
+                  className="animate-pulse mr-3"
+                />
+                <Skeleton
+                  height={36}
+                  width={100}
+                  className="animate-pulse mr-36"
+                />
               </div>
             ) : (
               <Footer.Brand
@@ -51,39 +59,60 @@ const FooterComponent = ({ isLoading }) => {
                 src="https://ecommerce.techinsights.guru/file/3d3e78e2-5f53-4d18-8818-7b01f9cef98c.png"
                 alt="Job Quick Logo"
                 name="Job Quick"
-                className="font-kantumruy uppercase text-secondary-300"
+                className="font-extrabold text-md sm:text-xl uppercase text-white pr-24"
               />
             )}
           </div>
           <div
-            className={`${fontClass} flex-grow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center`}
+            className={`${fontClass} flex-grow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-24`}
           >
             {isComponentLoading ? (
-              Array(3).fill(0).map((_, index) => (
-                <div key={index} className="text-center md:text-left mb-6 md:mb-0">
-                  <Skeleton height={24} width={150} className="mb-4 animate-pulse" />
-                  <Skeleton height={20} width={100} className="mb-2 animate-pulse" />
-                  <Skeleton height={20} width={100} className="animate-pulse" />
-                </div>
-              ))
+              Array(3)
+                .fill(0)
+                .map((_, index) => (
+                  <div
+                    key={index}
+                    className="text-center md:text-left mb-6 md:mb-0"
+                  >
+                    <Skeleton
+                      height={24}
+                      width={150}
+                      className="mb-4 animate-pulse"
+                    />
+                    <Skeleton
+                      height={20}
+                      width={100}
+                      className="mb-2 animate-pulse"
+                    />
+                    <Skeleton
+                      height={20}
+                      width={100}
+                      className="animate-pulse"
+                    />
+                  </div>
+                ))
             ) : (
               <>
                 <div className="text-center md:text-left mb-6 md:mb-0">
                   <Footer.Title
                     title={t("footer.website.name")}
-                    className="text-gray-300"
+                    className="text-gray-100 font-semibold text-lg"
                   />
-                  <Footer.LinkGroup col className="text-base">
-                    <Footer.Link href="#">{t("footer.website.jobs")}</Footer.Link>
-                    <Footer.Link href="#">{t("footer.website.media")}</Footer.Link>
+                  <Footer.LinkGroup col className="text-base text-gray-300 ">
+                    <Footer.Link href="#">
+                      {t("footer.website.jobs")}
+                    </Footer.Link>
+                    <Footer.Link href="#">
+                      {t("footer.website.media")}
+                    </Footer.Link>
                   </Footer.LinkGroup>
                 </div>
                 <div className="text-center md:text-left mb-6 md:mb-0">
                   <Footer.Title
                     title={t("footer.information.name")}
-                    className="text-gray-300"
+                    className="text-gray-100 font-semibold text-lg"
                   />
-                  <Footer.LinkGroup col className="text-base">
+                  <Footer.LinkGroup col className="text-base text-gray-300">
                     <Footer.Link href="#">
                       {t("footer.information.about-us")}
                     </Footer.Link>
@@ -92,12 +121,12 @@ const FooterComponent = ({ isLoading }) => {
                     </Footer.Link>
                   </Footer.LinkGroup>
                 </div>
-                <div className="text-center md:text-left mb-6 md:mb-0">
+                <div className="text-center md:text-left mb-6 md:mb-0 px-5">
                   <Footer.Title
                     title={t("footer.contact-us.name")}
-                    className="text-gray-300"
+                    className="text-gray-100 font-semibold text-lg"
                   />
-                  <Footer.LinkGroup col className="text-base">
+                  <Footer.LinkGroup col className="text-base text-gray-300">
                     <Footer.Link href="#">
                       {t("footer.contact-us.phone")}
                     </Footer.Link>
@@ -112,22 +141,29 @@ const FooterComponent = ({ isLoading }) => {
           <div className="w-full md:w-auto xl:w-96 flex justify-center md:justify-end">
             {isComponentLoading ? (
               <div>
-                <Skeleton height={24} width={250} className="mb-4 animate-pulse" />
-                <Skeleton height={20} width={200} className="mb-2 animate-pulse" />
+                <Skeleton
+                  height={24}
+                  width={250}
+                  className="mb-4 animate-pulse"
+                />
+                <Skeleton
+                  height={20}
+                  width={200}
+                  className="mb-2 animate-pulse"
+                />
               </div>
             ) : (
               <div>
                 <h3
-                  className={`${fontClass} mb-2 text-lg uppercase font-semibold text-gray-300`}
+                  className={`${fontClass} mb-2 text-lg uppercase font-semibold text-gray-100`}
                 >
                   {t("footer.notification")}
                 </h3>
                 <p
-                  className={`${fontClass} text-left mb-3 text-sm font-medium text-gray-400`}
+                  className={`${fontClass} text-left mb-3 text-sm font-medium text-gray-300`}
                 >
                   {t("footer.description")}
                 </p>
-                {/* Email Subscription Form */}
                 <form
                   action="https://app.convertkit.com/forms/4692392/subscriptions"
                   className="seva-form formkit-form"
@@ -158,7 +194,7 @@ const FooterComponent = ({ isLoading }) => {
                         </label>
                         <input
                           id="member_email"
-                          className={`${fontClass} formkit-input bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                          className={`${fontClass} formkit-input bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-md`}
                           name="email_address"
                           aria-label="Email Address"
                           placeholder={t("footer.email")}
@@ -167,7 +203,7 @@ const FooterComponent = ({ isLoading }) => {
                         />
                       </div>
                       <button data-element="submit" className="formkit-submit">
-                        <span className="px-6 py-3 text-sm font-semibold text-center text-white bg-primary-900 rounded-lg cursor-pointer hover:bg-primary-850 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <span className="px-6 py-3 text-sm font-semibold text-center text-white bg-primary-900 cursor-pointer hover:bg-primary-850 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 rounded-lg">
                           Subscribe
                         </span>
                       </button>
@@ -179,24 +215,62 @@ const FooterComponent = ({ isLoading }) => {
           </div>
         </div>
         <Footer.Divider />
-        <div className="w-full flex flex-col md:flex-row md:items-center md:justify-between">
+        <div className="w-full flex flex-col md:flex-row md:items-center md:justify-between mt-4">
           {isComponentLoading ? (
             <Skeleton height={20} width={150} className="animate-pulse" />
           ) : (
-            <Footer.Copyright href="#" by="Job Finder™" year={2024} />
+            <Footer.Copyright
+              href="#"
+              by="Job Finder™"
+              year={2024}
+              className="text-gray-200"
+            />
           )}
           <div className="mt-4 flex space-x-6 md:mt-0 md:justify-center">
             {isComponentLoading ? (
-              Array(5).fill(0).map((_, index) => (
-                <Skeleton key={index} circle={true} height={24} width={24} className="animate-pulse" />
-              ))
+              Array(5)
+                .fill(0)
+                .map((_, index) => (
+                  <Skeleton
+                    key={index}
+                    circle={true}
+                    height={24}
+                    width={24}
+                    className="animate-pulse"
+                  />
+                ))
             ) : (
               <>
-                <Footer.Icon href="#" icon={BsFacebook} aria-label="Facebook" />
-                <Footer.Icon href="#" icon={BsInstagram} aria-label="Instagram" />
-                <Footer.Icon href="#" icon={BsTwitter} aria-label="Twitter" />
-                <Footer.Icon href="#" icon={BsGithub} aria-label="Github" />
-                <Footer.Icon href="#" icon={BsDribbble} aria-label="Dribbble" />
+                <Footer.Icon
+                  href="#"
+                  icon={BsFacebook}
+                  aria-label="Facebook"
+                  className="text-gray-200"
+                />
+                <Footer.Icon
+                  href="#"
+                  icon={BsInstagram}
+                  aria-label="Instagram"
+                  className="text-gray-200"
+                />
+                <Footer.Icon
+                  href="#"
+                  icon={BsTwitter}
+                  aria-label="Twitter"
+                  className="text-gray-200"
+                />
+                <Footer.Icon
+                  href="#"
+                  icon={BsGithub}
+                  aria-label="Github"
+                  className="text-gray-200"
+                />
+                <Footer.Icon
+                  href="#"
+                  icon={BsDribbble}
+                  aria-label="Dribbble"
+                  className="text-gray-200"
+                />
               </>
             )}
           </div>
--- src/Components/NavbarComponent.jsx
+++ src/Components/NavbarComponent.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import useFontClass from "../common/useFontClass";
 import { Navbar, Dropdown, Avatar } from "flowbite-react";
-import { Link, NavLink, useLocation } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import LanguageDropdown from "../common/LanguageDropdown";
 import { useDispatch, useSelector } from "react-redux";
 import useLogout from "../common/useLogout";
@@ -12,8 +12,8 @@ import { fetchProfile } from "../redux/features/user/userSlice";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-const MenuList = ({ isLoading, menuList, fontClass }) => (
-  <Navbar.Collapse className={`md:flex justify-center space-x-4 ${isLoading ? "block" : "hidden"}`}>
+const MenuList = ({ isLoading, menuList, fontClass, isOpen, setIsOpen }) => (
+  <div className={`xl:flex justify-center space-x-10 ${isOpen ? "block" : "hidden"} xl:block transition-all duration-300 ease-in-out`}>
     {isLoading
       ? Array(5).fill(0).map((_, index) => (
           <div key={index} className="mx-2">
@@ -25,16 +25,16 @@ const MenuList = ({ isLoading, menuList, fontClass }) => (
             to={menu.path}
             className={({ isActive }) =>
               isActive
-                ? `${fontClass} text-lg font-medium text-white`
-                : `${fontClass} font-medium text-lg text-gray-300`
+                ? `${fontClass} text-lg font-medium text-white whitespace-nowrap`
+                : `${fontClass} font-medium text-lg text-gray-300 whitespace-nowrap`
             }
             key={index}
-            onClick={() => window.scrollTo(0, 0)}
+            onClick={() => setIsOpen(false)}
           >
             {menu.title}
           </NavLink>
         ))}
-  </Navbar.Collapse>
+  </div>
 );
 
 export default function NavbarComponent() {
@@ -44,7 +44,6 @@ export default function NavbarComponent() {
   const [localLoading, setLocalLoading] = useState(true);
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
   const logout = useLogout();
-  const location = useLocation();
 
   const dispatch = useDispatch();
   const { user, accessToken, isLoading: globalLoading } = useSelector((state) => state.user);
@@ -71,135 +70,144 @@ export default function NavbarComponent() {
     { path: "/contact-us", title: t("navbar.contact-us") },
   ];
 
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Navbar fluid className="bg-primary-800 shadow-md fixed top-0 left-0 right-0 z-50">
-      <div className="container mx-auto flex flex-wrap items-center justify-between py-2 px-4">
-        <Navbar.Brand href="/" className="flex items-center">
-          {isLoading ? (
-            <Skeleton circle height={36} width={36} className="mr-3 animate-pulse" />
-          ) : (
-            <img
-              alt="Logo"
-              loading="lazy"
-              width="36"
-              height="36"
-              decoding="async"
-              className="mr-3 w-9 h-9 object-contain"
-              src="https://ecommerce.techinsights.guru/file/3d3e78e2-5f53-4d18-8818-7b01f9cef98c.png"
-            />
-          )}
-          {isLoading ? (
-            <Skeleton height={20} width={100} className="animate-pulse" />
-          ) : (
-            <div className="font-extrabold text-md sm:text-xl uppercase text-secondary-300 dark:text-blue-600">
-              Job Quick
-            </div>
-          )}
-        </Navbar.Brand>
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap items-center justify-between py-2 gap-4">
+          <Navbar.Brand href="/" className="flex items-center whitespace-nowrap">
+            {isLoading ? (
+              <Skeleton circle height={30} width={30} className="mr-3 animate-pulse" />
+            ) : (
+              <img
+                alt="Logo"
+                loading="lazy"
+                width="30"
+                height="30"
+                decoding="async"
+                className="mr-3 w-7 h-7 object-contain"
+                src="https://ecommerce.techinsights.guru/file/3d3e78e2-5f53-4d18-8818-7b01f9cef98c.png"
+              />
+            )}
+            {isLoading ? (
+              <Skeleton height={20} width={80} className="animate-pulse" />
+            ) : (
+              <div className="font-extrabold text-md sm:text-lg uppercase text-secondary-300 dark:text-blue-600 whitespace-nowrap">
+                Job Quick
+              </div>
+            )}
+          </Navbar.Brand>
 
-        <div className="flex flex-grow justify-center order-2 md:order-1 w-full md:w-auto">
-          <MenuList isLoading={isLoading} menuList={menuList} fontClass={fontClass} />
+          <div className="hidden xl:flex flex-grow justify-center">
+            <MenuList isLoading={isLoading} menuList={menuList} fontClass={fontClass} isOpen={isOpen} setIsOpen={setIsOpen} />
+          </div>
+
+          <div className="relative flex items-center gap-x-4 xl:gap-x-8 whitespace-nowrap">
+            <LanguageDropdown fontClass={fontClass} />
+            {isLoading ? (
+              <Skeleton circle height={35} width={35} className="animate-pulse" />
+            ) : isAuthenticated ? (
+              <Dropdown
+                arrowIcon={false}
+                inline
+                label={<Avatar alt="User settings" img={user?.avatar} rounded />}
+              >
+                <Dropdown.Header>
+                  <span className="block text-sm text-left">{user?.username}</span>
+                  <span className="block truncate text-sm font-medium">{user?.email}</span>
+                </Dropdown.Header>
+                <Dropdown.Item as={Link} to="/profile">
+                  Settings
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={logout}>Sign out</Dropdown.Item>
+              </Dropdown>
+            ) : (
+              <>
+                <NavLink to="/login">
+                  <button
+                    type="button"
+                    className="flex items-center px-4 py-2 text-center font-medium text-white bg-primary-900 hover:bg-primary-850 focus:ring-primary-650 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800 rounded-lg focus:ring-2 whitespace-nowrap"
+                  >
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 512 512"
+                      className="h-5 w-5 mr-2"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="32"
+                        d="M192 176v-40a40 40 0 0140-40h160a40 40 0 0140 40v240a40 40 0 01-40 40H240c-22.09 0-48-17.91-48-40v-40"
+                      ></path>
+                      <path
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="32"
+                        d="M288 336l80-80-80-80M80 256h272"
+                      ></path>
+                    </svg>
+                    <span className={`${fontClass} hidden xl:inline font-medium uppercase`}>
+                      {t("auth.login")}
+                    </span>
+                  </button>
+                </NavLink>
+
+                <NavLink to="/register">
+                  <button
+                    type="button"
+                    className="flex items-center px-4 py-2 text-center font-medium text-white bg-primary-900 hover:bg-primary-850 focus:ring-primary-650 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800 rounded-lg focus:ring-2 whitespace-nowrap"
+                  >
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 640 512"
+                      className="h-5 w-5 mr-2"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M624 208h-64v-64c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v64h-64c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h64v64c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-64h64c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm-400 48c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path>
+                    </svg>
+                    <span className={`${fontClass} hidden xl:inline font-medium uppercase`}>
+                      {t("auth.register")}
+                    </span>
+                  </button>
+                </NavLink>
+              </>
+            )}
+            <button
+              onClick={toggleNavbar}
+              className="xl:hidden ml-3 p-2 text-white bg-primary-900 hover:bg-primary-850 focus:ring-primary-650 rounded-lg focus:ring-2"
+              aria-label="Toggle navigation"
+              aria-expanded={isOpen}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </button>
+          </div>
         </div>
-
-        <div className="relative flex order-1 md:order-2 items-center gap-x-6">
-          <LanguageDropdown fontClass={fontClass} />
-          {isLoading ? (
-            <Skeleton circle height={40} width={40} className="animate-pulse" />
-          ) : isAuthenticated ? (
-            <Dropdown
-              arrowIcon={false}
-              inline
-              label={<Avatar alt="User settings" img={user?.avatar} rounded />}
-            >
-              <Dropdown.Header>
-                <span className="block text-sm text-left">{user?.username}</span>
-                <span className="block truncate text-sm font-medium">{user?.email}</span>
-              </Dropdown.Header>
-              <Dropdown.Item as={Link} to="/profile">
-                Settings
-              </Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item onClick={logout}>Sign out</Dropdown.Item>
-            </Dropdown>
-          ) : (
-            <>
-              <NavLink to="/login">
-                <button
-                  type="button"
-                  className="flex items-center px-5 py-3 text-center font-medium text-white bg-primary-900 hover:bg-primary-850 focus:ring-primary-650 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800 rounded-full focus:ring-2"
-                >
-                  <svg
-                    stroke="currentColor"
-                    fill="currentColor"
-                    strokeWidth="0"
-                    viewBox="0 0 512 512"
-                    className="h-5 w-5 mr-2"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="32"
-                      d="M192 176v-40a40 40 0 0140-40h160a40 40 0 0140 40v240a40 40 0 01-40 40H240c-22.09 0-48-17.91-48-40v-40"
-                    ></path>
-                    <path
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="32"
-                      d="M288 336l80-80-80-80M80 256h272"
-                    ></path>
-                  </svg>
-                  <span className={`${fontClass} hidden lg:inline font-medium uppercase`}>
-                    {t("auth.login")}
-                  </span>
-                </button>
-              </NavLink>
-
-              <NavLink to="/register">
-                <button
-                  type="button"
-                  className="flex items-center px-5 py-3 text-center font-medium text-white bg-primary-900 hover:bg-primary-850 focus:ring-primary-650 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800 rounded-full focus:ring-2"
-                >
-                  <svg
-                    stroke="currentColor"
-                    fill="currentColor"
-                    strokeWidth="0"
-                    viewBox="0 0 640 512"
-                    className="h-5 w-5 mr-2"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M624 208h-64v-64c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v64h-64c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h64v64c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-64h64c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm-400 48c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path>
-                  </svg>
-                  <span className={`${fontClass} hidden lg:inline font-medium uppercase`}>
-                    {t("auth.register")}
-                  </span>
-                </button>
-              </NavLink>
-            </>
-          )}
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="md:hidden ml-3 p-2 text-white bg-primary-900 hover:bg-primary-850 focus:ring-primary-650 rounded-lg focus:ring-2"
-            aria-label="Toggle navigation"
-            aria-expanded={isOpen}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
+        <div className="block xl:hidden mx-auto">
+          <MenuList isLoading={isLoading} menuList={menuList} fontClass={fontClass} isOpen={isOpen} setIsOpen={setIsOpen} />
         </div>
       </div>
     </Navbar>
