2456e231319f525af59798fd31d7df4ac239d5cc
piseth sattya <pisethsattya33@gmail.com>
2024-07-18 01:02:53 +0700

    seo and performance

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,34 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      viteCompression({
+        verbose: true,
+        disable: false,
+        deleteOriginFile: false,
+        threshold: 10240,
+        algorithm: "gzip",
+        ext: ".gz",
+      }),
+      viteCompression({
+        verbose: true,
+        disable: false,
+        deleteOriginFile: false,
+        threshold: 10240,
+        algorithm: "brotliCompress",
+        ext: ".br",
+      }),
+    ],
     define: {
-      'process.env.NODE_ENV': `"${mode}"`,
+      "process.env.NODE_ENV": `"${mode}"`,
     },
     build: {
-      outDir: 'dist',
+      outDir: "dist",
     },
     server: {
       port: 3000,
