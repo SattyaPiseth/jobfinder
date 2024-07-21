6a37f6545d0d5241e1f5f9e8be9f258f909e2a9f
piseth sattya <pisethsattya33@gmail.com>
2024-07-22 00:05:43 +0700

    testing

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,18 @@
 # Ensure the script fails if any command fails
 set -e
 
-# Generate a unique container name based on timestamp
-CONTAINER_NAME="my_container_$(date +%s)"
+# Prompt the user to enter a unique container name or generate one based on timestamp
+read -p "Enter a name container or press enter to use a timestamp-based name: " CUSTOM_CONTAINER_NAME
+if [ -z "$CUSTOM_CONTAINER_NAME" ]; then
+    CONTAINER_NAME="my_container_$(date +%s)"
+else
+    CONTAINER_NAME="$CUSTOM_CONTAINER_NAME"
+fi
 
 # Prompt the user for Docker Hub credentials
 read -p "Enter your Docker Hub username: " USERNAME
 read -sp "Enter your Docker Hub password: " PASSWORD
-echo
+echo  # This echo command is to move to the next line after password input
 
 # Prompt for Docker repository and tag
 read -p "Enter the Docker repository name (e.g., username/repository): " REPOSITORY
@@ -29,6 +34,13 @@ docker compose build
 # Push the Docker image
 echo "Pushing image to Docker Hub..."
 docker push $REPOSITORY:$TAG
+
+# Check if a container with the specified name already exists
+if [ $(docker ps -aq -f name=^$CONTAINER_NAME$) ]; then
+    echo "Container with name $CONTAINER_NAME already exists. Stopping and removing..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+fi
 
 # Run the Docker container
 echo "Running the Docker container..."
