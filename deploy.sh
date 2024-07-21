3d2ec669315f10834df7c053b0ac19a5730336a2
piseth sattya <pisethsattya33@gmail.com>
2024-07-21 23:45:00 +0700

    testing

--- deploy.sh
+++ deploy.sh
@@ -30,28 +30,6 @@ docker compose build
 echo "Pushing image to Docker Hub..."
 docker push $REPOSITORY:$TAG
 
-# Function to check if a port is in use
-function is_port_in_use() {
-    lsof -i:$1 > /dev/null
-}
-
-# Function to stop any container using the specified port
-function stop_container_using_port() {
-    local PORT=$1
-    local CONTAINER_ID=$(lsof -i:$PORT -t)
-    if [ ! -z "$CONTAINER_ID" ]; then
-        echo "Stopping container using port $PORT (Container ID: $CONTAINER_ID)..."
-        docker stop $CONTAINER_ID
-        docker rm $CONTAINER_ID
-    fi
-}
-
-# Check if port 8086 is in use and stop the container using it if necessary
-PORT=8086
-if is_port_in_use $PORT; then
-    stop_container_using_port $PORT
-fi
-
 # Run the Docker container
 echo "Running the Docker container..."
 docker run -d -p 8086:80 --name $CONTAINER_NAME $REPOSITORY:$TAG
