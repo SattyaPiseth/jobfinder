59e09ef2121ad1cc9b7a1b66017960296ebe650f
piseth sattya <pisethsattya33@gmail.com>
2024-08-13 12:25:26 +0700

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -42,10 +42,10 @@ function App() {
   return (
     <>
       <Metadata
-        title="Home"
-        description="Job Quick, established in 2024 by a group of ambitious undergraduate students from the Center for Advanced Science and Technology Development, is an innovative online platform designed to simplify the job search process. Our platform empowers users to effortlessly find, apply to, and track job opportunities, making the job search experience more streamlined and efficient."
-        author="Job Quick Team"
-        keywords="job search, apply jobs, job tracking, employment, careers, job opportunities, job portal, online jobs, Job Quick"
+        // title="Home"
+        // description="Job Quick, established in 2024 by a group of ambitious undergraduate students from the Center for Advanced Science and Technology Development, is an innovative online platform designed to simplify the job search process. Our platform empowers users to effortlessly find, apply to, and track job opportunities, making the job search experience more streamlined and efficient."
+        // author="Job Quick Team"
+        // keywords="job search, apply jobs, job tracking, employment, careers, job opportunities, job portal, online jobs, Job Quick"
         thumbnail="https://cdn.builder.io/api/v1/image/assets/TEMP/0a80561409e726af6d7ed574172e4be459d2b6e25d22d27c2002b385768104b1?apiKey=391ff68a63584b0181b4aa51e20262f0&&apiKey=391ff68a63584b0181b4aa51e20262f0"
         url="https://jobquick.techinsights.guru/"
         type="website"
