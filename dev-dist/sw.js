eae4e0fa1afc7c77ac456054b71fdc64cb8501fe
ChaoKimhay <Chaokimhay>
2024-08-13 15:18:01 +0700

    route media

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-2dac8ca2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h6k965f7lto"
+    "revision": "0.fg3do2njpd8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
