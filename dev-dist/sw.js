98eeb802ec36476a50a1916f63afbbb27b631199
piseth sattya <pisethsattya33@gmail.com>
2024-08-13 19:29:36 +0700

    testing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-2dac8ca2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jtvophuoqi8"
+    "revision": "0.o8a0qotpbp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
