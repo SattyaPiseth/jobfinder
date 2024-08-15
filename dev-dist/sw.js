6e00175caa966d70a1eb541a9ee74d928120ea16
ChaoKimhay <Chaokimhay>
2024-08-15 15:29:31 +0700

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-2dac8ca2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.64fooeo377g"
+    "revision": "0.u9onf8m978"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
